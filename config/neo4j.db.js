f74d992a6e6231e08e45226eeee369d3ed093a65
Walid Said <wsaid@avans.nl>
2017-12-06 00:47:59 +0100

    Fixed config path

--- config/neo4j.db.js
+++ config/neo4j.db.js
@@ -1,5 +1,5 @@
 const neo4j = require('neo4j-driver').v1;
-const config = require('./config/env/env');
+const config = require('./env/env');
 
 const driver = neo4j.driver("bolt://hobby-dehocbifaghcgbkeihidlial.dbs.graphenedb.com:24786", neo4j.auth.basic(config.env.dbDatabase, config.env.neo4jDatabasePassword));
 
