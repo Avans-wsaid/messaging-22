077ecabe294581147a1be1d18c60dd9277241796
W Said <wsaid@avans.nl>
2017-12-15 12:02:38 +0100

    Update neo4j.db.js

--- config/neo4j.db.js
+++ config/neo4j.db.js
@@ -1,6 +1,6 @@
 const neo4j = require('neo4j-driver').v1;
 const config = require('./env/env');
 
-const driver = neo4j.driver("bolt://hobby-dehocbifaghcgbkeihidlial.dbs.graphenedb.com:24786", neo4j.auth.basic(config.env.dbDatabase, config.env.neo4jDatabasePassword));
+const driver = neo4j.driver("bolt://hobby-bpjpjckeehocgbkekibajjal.dbs.graphenedb.com:24786", neo4j.auth.basic(config.env.dbDatabase, config.env.neo4jDatabasePassword));
 
 module.exports = driver;
